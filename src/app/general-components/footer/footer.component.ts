60c526ee5354f5a06e65590f0e1e2a44e8f73841
Anne de Graaff <a.d.graaff@hotmail.com>
2020-05-21 21:55:47 +0200

    Added Header and Footer components

---
+++ src/app/general-components/footer/footer.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+  //First item of array is button text
+  //Second item of array is id
+  navigation = [["Home", 0], ["Contact",1], ["Overig", 2]]
+  apple = new Date().getFullYear()
+  ngOnInit(): void {
+  }
+
+  handleClick(id): void {
+    switch(id) {
+      case 0:
+        //handle go to homepage
+        break;
+      case 1:
+        //handle go contact info
+        break;
+      case 2:
+        //handle go overig
+        break;
+    }
+  }
+
+}
