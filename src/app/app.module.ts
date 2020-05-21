6ec661d576cb7cf938b042f2bdd32a06bdc1eb2c
Anne de Graaff <a.d.graaff@hotmail.com>
2020-05-21 22:00:31 +0200

    Updated imports. Removed seperate components and added module. Because that way we don't have to add all components separately

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './general-components/header/header.component';
 import { FooterComponent } from './general-components/footer/footer.component';
+import { GeneralComponentsModule } from './general-components/general-components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GeneralComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
