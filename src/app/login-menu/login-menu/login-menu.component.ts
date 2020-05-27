9ff4c7d74c55301465bd691820235b2b638cb6e1
Pieter Beens <beens.pieter@gmail.com>
2020-05-27 12:33:53 +0200

    Login validation added (hardcoded details)

--- src/app/login-menu/login-menu/login-menu.component.ts
+++ src/app/login-menu/login-menu/login-menu.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './login-menu.component.html',
   styleUrls: ['./login-menu.component.scss']
 })
+
 export class LoginMenuComponent implements OnInit {
 
   mail: string = "";
@@ -17,7 +18,25 @@ export class LoginMenuComponent implements OnInit {
   }
 
   validateResponses() {
-    this.result = this.mail;
+    let validated = false;
+    mockDetails.forEach(element => {
+      if (element[0] == this.mail && element[1]  == this.password) validated = true;
+    });
+    if (validated) this.result = "Haxx0red";
+    else this.result = "Incorrect details";
+  }
+
+  validateResponsesDATABASE() {
+    let theUrl = "https://www.sodine.nl/" + this.mail + this.password;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
   }
 
 }
+
+const mockDetails = [
+  ["Anna", "hummer"],
+  ["admin", "123"]
+];
