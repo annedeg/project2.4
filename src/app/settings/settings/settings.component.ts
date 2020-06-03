b4ed7b27181c6ddf3b75774da003907634374fbc
Nicky_de_Scholman <p.nick_scholma@hotmail.com>
2020-06-03 18:23:02 +0200

    Just a start, a lot of fucntionality is missing and it needs to be tied to the API for a lot to work.

--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -7,9 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  usernameEdit : boolean = false;
+  deleteConfirmation : boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  usernameEditToggle() {
+    if (this.usernameEdit) {
+      this.usernameEdit = false;
+    } else {
+      this.usernameEdit = true;
+    }
+  }
+
+  editUsername() {
+    //TODO actually change the username if it's available
+    this.usernameEditToggle();
+  }
+
+  deleteConfirmToggle() {
+    if (this.deleteConfirmation) {
+      this.deleteConfirmation = false;
+    } else {
+      this.deleteConfirmation = true;
+    }
+  }
+
+  deletionPasswordCheck(pass : String) {
+    if (pass.length == 0) {
+      //Do nothing
+    }
+    else {
+      if (pass === "GenericPassword") { //TODO require the actual password
+        this.deleteData();
+      }
+      else {
+        //TODO logical way to show that the user filled in a fall password, preferably without another goddamn boolean
+      }
+    }
+  }
+
+  deleteData() {
+    //TODO Actually delete the data, and take the user back to the log in page
   }
 
 }
